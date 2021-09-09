b861818060c443795edd444d110fa350e9126764
Md Shoaib <shoaib10oct@gmail.com>
2021-09-09 08:24:53 +0530

    while Loop Basics

---
+++ src/myWhileLoop/myWhileLoop.kt
@@ -0,0 +1,16 @@
+package myWhileLoop
+
+/*
+* Author: Md Shoaib
+* Date: 09/09/2021
+* Desc: Basics of While loop
+*/
+
+fun main() {
+    var number = 1
+
+    while (number <= 20){
+        println(number)
+        number++
+    }
+}
