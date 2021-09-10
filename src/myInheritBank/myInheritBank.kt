af20608f9a291a594fca6072601acfd87ff4b8ea
Md Shoaib <shoaib10oct@gmail.com>
2021-09-10 07:45:30 +0530

    Inheritence, open and override

---
+++ src/myInheritBank/myInheritBank.kt
@@ -0,0 +1,41 @@
+package myInheritBank
+
+/*
+* Author: Md Shoaib
+* Date: 10/09/2021
+* Desc: Inheritance, Open and override
+*/
+
+open class Account(open val name: String, open val  balance: Int){
+    fun printDetails(){
+        println("Bank Person details are: [name: $name, balance: $balance]")
+    }
+
+    fun welcomeMsg(){
+        println("Hello $name, Welcome to our Bank")
+    }
+
+   open fun accountTypeMsg(){
+        println("Hey $name, Welcome to simple account")
+    }
+}
+
+class SavingAccount(override val name: String, override val  balance: Int) : Account(name, balance){
+    override fun accountTypeMsg() {
+        println("Hey $name, Welcome to Saving account")
+    }
+
+    fun addInterest(){
+        println("I will give you 12% of interest")
+    }
+}
+
+
+fun main() {
+    var adam = Account(name = "Adam", balance = 5000)
+    adam.accountTypeMsg()
+
+    var ross = SavingAccount(name = "Ross", balance = 500)
+    ross.accountTypeMsg()
+    ross.addInterest()
+}
