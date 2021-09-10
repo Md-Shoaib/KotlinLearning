22b2b8fed7ce812a8b0541d43c4edf4307a5b8ef
Md Shoaib <shoaib10oct@gmail.com>
2021-09-10 06:53:53 +0530

    Basics of Classes and Objects

---
+++ src/myClass/myClass.kt
@@ -0,0 +1,22 @@
+package myClass
+
+/*
+* Author: Md Shoaib
+* Date: 10/09/2021
+* Desc: Classes and Objects
+*/
+
+class Man {
+    var name: String = "Joey"
+    var age: Int = 27
+}
+
+fun main() {
+
+//    Creating class and its constructer
+    var man = Man()
+
+    println(man.name)
+    println(man.age)
+
+}
